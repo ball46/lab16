e03d850d918f844a1f9f1597e2f23d7490836c34
charnchol <khumball46@gmail.com>
2022-01-14 09:58:58 +0700

    lab 16-4 complete

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,37 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &A,int &B,int &C,int &D){
+	int a[4];
+	a[0] = A , a[1] = B , a[2] = C , a[3] =D;
+
+	int b[4];
+
+	int e = rand()% 4;
+	b[0] = a[e];
+	a[e] = 0;
+
+	int f = 0;
+	do{
+		f = rand()%4;
+	}while(a[f] == 0) ;
+	b[1] = a[f];
+	a[f] = 0;
+
+	int g = 0;
+	do{
+		g = rand()% 4;
+	}while(a[g] == 0) ;
+	b[2] = a[g];
+	a[g] = 0;
+
+	int h = 0;
+	do{
+		h = rand()% 4;
+	}while(a[h] == 0) ;
+	b[3] = a[h];
+	a[h] = 0;
+
+	A = b[0] , B = b[1] , C =b[2] , D = b[3];
+}
